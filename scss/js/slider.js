0aa95d34fc4d1f2bcd959a05bbc6af787dc391a9
aldrin1122 <0318-1190@lspu.edu.ph>
2024-10-10 15:08:02 +0800

    first commit

---
+++ scss/js/slider.js
@@ -0,0 +1,10 @@
+var slider = tns({
+  container: ".findMe__background--slider",
+  items: 6,
+  speed: 500,
+  slideBy: 1,
+  containerControls: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+  rewind: true,
+});
